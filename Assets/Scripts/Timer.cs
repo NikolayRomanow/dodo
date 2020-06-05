238b982b6b026f557879d7f927708660a670810e
NikolayRomanow <system.program.code@gmail.com>
2020-06-05 18:56:25 +0500

    Череновой вариант базовой одиночки

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject PunelOff;
+    public Text text;
+    public float TimeLeft;
+    private float TimeYes;
+    public Transform Bird;
+    public Transform PointEnd;
+    private int _count = 0;
+
+    private void Update()
+    {
+        TimeLeft += Time.deltaTime;
+        if (Bird.position == PointEnd.position && _count == 0)
+        {
+            TimeYes = TimeLeft;
+            _count++;
+        }
+        if (Bird.position == PointEnd.position)
+        {
+            Panel.SetActive(true);
+            PunelOff.SetActive(false);
+            text.text = "Ваше время: " + Convert.ToString(TimeYes);
+        }
+        
+    }
+}
--- Assets/Scripts/Victorine.cs
+++ Assets/Scripts/Victorine.cs
@@ -11,6 +11,7 @@ public class Victorine : MonoBehaviour
     public QuestionList[] Questions;
     public Text[] AnswersText;
     public Text qText;
+    public AIMovie ScriptMoving;
 
     private List<object> qList;
     private QuestionList crntQ;
@@ -46,10 +47,15 @@ public class Victorine : MonoBehaviour
     public void AnswersButtons(int index)
     {
         if (AnswersText[index].text.ToString() == crntQ.Answer[0])
+        {
             Debug.Log("+1");
+            ScriptMoving.Speed += 0.05f;
+        }
+
         else
         {
             Debug.Log("-1");
+            ScriptMoving.Speed -= 0.05f;
         }
         qList.RemoveAt(randQ);
         _questionGenerate();
